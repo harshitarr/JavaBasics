25252d4170b2bd278385ccd3bcab77476a55a203
Harshita <harshitarera@gmail.com>
2024-12-28 14:12:18 +0530

    commit

---
+++ Find.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Find
+{
+  public static void main(String args[])
+  {
+
+  String dum[]={"meow","bow"};
+  for(int i=0;i<4;i++)
+  {
+    System.out.println(dum[i]);
+  }
+
+  for(String var:dum)
+  {
+    System.out.println(var);
+  }
+
+
+}
+}
+
