6eb4ea7dfb0f99fc8a645942c37f9c9a11d20230
Harshita <harshitarera@gmail.com>
2025-01-07 00:18:35 +0530

    commit

---
+++ java/demonstration_21.java
@@ -0,0 +1,9 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class demonstration_21 extends Applet{
+
+public void paint(Graphics g){
+
+g.drawstring("hello world",150,150);
+}}
---
+++ java/meow.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Meow
+{
+    public static void main(String args[])
+    {
+        System.out.println("meow");
+    }
+}
