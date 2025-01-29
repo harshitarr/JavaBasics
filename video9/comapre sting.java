e2cfb8e57b198c1baf0f5ca4640ca48040134a58
Harshita <harshitarera@gmail.com>
2025-01-30 00:03:03 +0530

    javacommit

---
+++ video9/comapre sting.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class video8
+{
+public static void main(String args[])
+{
+    String a="apple";
+    String b="apple";
+    System.out.println(a==b);
+    String f1=new String("apple");
+    String f2=new String("apple");
+     System.out.println(f1==f2);
+  
+}
+
+}
+
+output
+true
+false
