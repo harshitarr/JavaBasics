2ee0c24fb1af3c4e4bc7a7317b8dd27bb534987b
Harshita <harshitarera@gmail.com>
2025-02-02 23:33:21 +0530

    m

---
+++ video9/string.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class video8
+{
+public static void main(String args[])
+{
+    
+   
+    String f1=new String("apple");
+    String f2=new String("apple");
+     System.out.println(f1.equals(f2));
+  
+}
+
+}
